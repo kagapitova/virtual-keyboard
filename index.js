64cc9b5a1e0e84e8eba679444859a498238ef7e2
kseniaagapitova <kagapitova@inbox.ru>
2023-04-27 02:16:32 +0300

    refactor:body includes only scripts

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const keyboardPlace = document.querySelector("#keyboard");
+/*Render keyboard==============================================================*/
+const keyContainer = document.createElement("div");
+keyContainer.id = "keyboard1";
+document.body.append(keyContainer)
+const keyboardPlace = document.querySelector("#keyboard1");
 
 function initKeyboard() {
     let htmlString = "";
@@ -7,9 +11,16 @@ function initKeyboard() {
         if( i == 14 || i == 29 || i == 42 || i == 54 ) {
             htmlString += `<div class="new-row"></div>`;
         }
-        htmlString += `<div class="keyboard__key">${keys.keys[i].code}</div>`;
+        htmlString += `<div class="keyboard__key">${keys.keys[i].text.ru}</div>`;
     }
     keyboardPlace.innerHTML = htmlString;
 }
 
 initKeyboard()
+
+/*Language switcher==============================================================*/
+let lang = 'ru';
+function swichLanguage() {
+        lang = (lang === 'ru') ? 'en' : 'ru';
+        initKeyboard()
+}
